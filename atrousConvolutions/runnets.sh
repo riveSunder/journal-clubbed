99536002ffd180da4a37b187aa9e3f4001161e1a
radlr <thescinder@gmail.com>
2018-06-07 00:29:40 +0100

    fix bugs for multiscaleNet.py, now runs

--- atrousConvolutions/runnets.sh
+++ atrousConvolutions/runnets.sh
@@ -1,9 +1,12 @@
+python multiscaleNet.py --model='UNet' --maxSteps=400 
 
-python atrousNet.py --restore=False --useAtrous=True --useUNet=False --maxSteps=500 --dispIt=20 --lR=3e-5 --poolStride=4
+python multiscaleNet.py --model='MDAC' --maxSteps=400
 
-python atrousNet.py --restore=False --useAtrous=False --useUNet=False --maxSteps=800 --dispIt=20 --lR=3e-5 --poolStride=4
+#python atrousNet.py --restore=False --useAtrous=True --useUNet=False --maxSteps=1200 --dispIt=20 --lR=3e-4 --poolStride=4
 
-python atrousNet.py --restore=False --useAtrous=False --useUNet=True --maxSteps=800 --dispIt=20 --lR=3e-5 --poolStride=4
+#python atrousNet.py --restore=False --useAtrous=False --useUNet=False --maxSteps=1200 --dispIt=20 --lR=3e-4 --poolStride=4
 
-python atrousNet.py --restore=False --useAtrous=True --useUNet=True --maxSteps=800 --dispIt=20 --lR=3e-5 --poolStride=4
+#python atrousNet.py --restore=False --useAtrous=False --useUNet=True --maxSteps=1200 --dispIt=20 --lR=3e-4 --poolStride=4
+
+#python atrousNet.py --restore=False --useAtrous=True --useUNet=True --maxSteps=1200 --dispIt=20 --lR=3e-4 --poolStride=4
 
