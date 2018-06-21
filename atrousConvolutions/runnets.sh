71967bd861df8c6b3d16aef07783bddf4645c0b9
Rive Sunder <rivesunder@protonmail.com>
2018-06-21 22:29:36 +0100

    add output data

--- atrousConvolutions/runnets.sh
+++ atrousConvolutions/runnets.sh
@@ -1,8 +1,8 @@
-python multiscaleNet.py --model='UNet' --maxSteps=5000 --dispIt=100 --batchSize=8 --lR=9e-6 --restore=False
+python multiscaleNet.py --model='MDAC' --maxSteps=7001 --dispIt=25 --batchSize=8 --lR=9e-6 --restore=False
 
-python multiscaleNet.py --model='MDAC' --maxSteps=5000 --dispIt=100 --batchSize=8 --lR=9e-6 --restore=False
+python multiscaleNet.py --model='UNet' --maxSteps=7001 --dispIt=25 --batchSize=8 --lR=9e-6 --restore=False
 
-python simplyFullyCNNet.py --maxSteps=5000 --dispIt=100 --batchSize=8 --lR=9e-6 --restore=False
+python simplyFullyCNNet.py --maxSteps=7001 --dispIt=25 --batchSize=8 --lR=9e-6 --restore=False
 
 #python atrousNet.py --restore=False --useAtrous=True --useUNet=False --maxSteps=1200 --dispIt=20 --lR=3e-4 --poolStride=4
 
